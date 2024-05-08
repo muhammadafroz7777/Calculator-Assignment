111a632d5a5a3655d57fbb0dae12e1ab3e9e13e6
muhammadafroz7777 <muhammadafroz0320@gmail.com>
2024-05-08 19:51:06 +0500

    conti

--- app.js
+++ app.js
@@ -1,20 +1,59 @@
-var input = document.getElementById("num");
+// var input = document.getElementById("num");
 // var num1 = document.getElementById("num1");
 // var num2 = document.getElementById("num2");
-var result = document.getElementById("result");
+// var result = document.getElementById("result");
 //declaration
-function isEven(num) {
-  if (num % 2 == 0) {
-    return true;
-  } else {
-    return false;
-  }
+// function isEven(num) {
+//   if (num % 2 == 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// function checkNumber() {
+//   if (isEven(input.value)) {
+//     result.innerText = "Number is Even";
+//   } else {
+//     result.innerText = "Number is Odd";
+//   }
+// }
+
+// --------------------
+
+
+// function calculate(sign = "+") {
+//   var num1Val = num1.value;
+//   var num2Val = num2.value;
+//   if (sign == "+")
+//     return (result.innerText = parseInt(num1Val) + parseInt(num2Val));
+//   if (sign == "-") return (result.innerText = num1Val - num2Val);
+//   if (sign == "x") return (result.innerText = num1Val * num2Val);
+//   // console.log("---------------------");
+
+//   if (sign == "/") return (result.innerText = num1Val / num2Val);
+//   if (sign == "%") return (result.innerText = num1Val / num2Val * 100);
+// }
+
+// ---------------------
+
+
+const display = document.getElementById("display");
+
+
+function appappandTodisplay(input) {
+  display.value += input;
 }
 
-function checkNumber() {
-  if (isEven(input.value)) {
-    result.innerText = "Number is Even";
-  } else {
-    result.innerText = "Number is Odd";
-  }
+
+function cleardisplay(){
+  display.value = "";
 }
+
+
+function calculate(){
+  display.value = eval(display.value);
+}
+
+
+
